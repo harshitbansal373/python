847b02d26ee17996f03d8a9a8bbb37720fdbf83e
harshitbansal373 <harshitbansal373@gmail.com>
2019-06-16 16:48:35 +0530

    pattern13

--- patterns/13.py
+++ patterns/13.py
@@ -1,13 +1,13 @@
 n=int(input("enter number of rows: "))
-for i in range(n,0,-1):
+for i in range(1,n+1):
   for j in range(n-i+1,n+1):
    print(j,end='')
   print()
 
 '''output
-12345
-2345
-345
-45
 5
+45
+345
+2345
+12345
 '''
