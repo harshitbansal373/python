f13a866ecbeab2b0f65a6d63002204e6047f5531
harshitbansal373 <harshitbansal373@gmail.com>
2019-06-18 14:30:59 +0530

    pattern19

--- patterns/19.py
+++ patterns/19.py
@@ -1,14 +1,11 @@
 n=int(input())
 for i in range(1,n+1):
     if i!=n:
-      if i>1:
-        print('*',' '*(n-2),'*',sep='')
-      else:
-        print('*',' '*(n-2),'*'*n,sep='')
+        print('*',' '*(n-2),'*'*n if i==1 else '*',sep='')
     else:
       print('*'*(2*n-1),sep='')
 
-for i in range(n,0,-1):
+for i in range(2,n+1):
     if i==n:
       print('*'*n,' '*(n-2),'*',sep='')
     else:
