75d97348aadf6314306f1009931930e536ed162e
harshitbansal373 <harshitbansal373@gmail.com>
2019-06-19 21:26:12 +0530

    division concept based problem

---
+++ division.py
@@ -0,0 +1,35 @@
+'''
+problem---
+
+Task 
+Read two integers and print two lines. The first line should contain integer division, a//b . The second line should contain float division, a/b .
+
+You don't need to perform any rounding or formatting operations.
+
+Input Format
+
+The first line contains the first integer, a. The second line contains the second integer, b.
+
+Output Format
+
+Print the two lines as described above.
+
+Sample Input 0
+
+4
+3
+
+Sample Output 0
+
+1
+1.33333333333
+
+'''
+
+#code is here:-
+
+a = int(input())
+b = int(input())
+print(a//b)
+print(a/b)
+
