dbd7f4ee33e0f95090cadabaeb3d869a69866de2
harshitbansal373 <harshitbansal373@gmail.com>
2019-08-19 21:35:50 +0530

    counter

---
+++ colletions/counter.py
@@ -0,0 +1,5 @@
+import collections
+print(collections.Counter(['a','b','c','a','b','b']))
+print(collections.Counter({'a':2,'b':3,'c':1}))
+print(collections.Counter(a=2,b=3,c=1))
+print(collections.Counter({1,2,3,3,3,2,1,2,3}))
